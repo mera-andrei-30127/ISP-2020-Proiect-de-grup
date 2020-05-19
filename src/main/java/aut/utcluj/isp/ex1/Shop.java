b8002d3048e0b3be750297452fe0f3583345d420
mera-andrei <Andrei.MERA@student.utcluj.ro>
2020-05-19 20:17:48 +0300

    ex1

---
+++ src/main/java/aut/utcluj/isp/ex1/Shop.java
@@ -0,0 +1,27 @@
+package aut.utcluj.isp.ex1;
+
+/**
+ * @author stefan
+ */
+public class Shop {
+    private String name;
+    private String city;
+
+    public Shop(String name, String city) {
+        this.city = city;
+        this.name = name;
+    }
+
+    public Shop(String name) {
+        this.city = "";
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
